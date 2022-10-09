0e7725f6581e0114cfde4899aeda3e43cdac6db8
Joshua <joshua.beidham@griffithuni.edu.au>
2022-10-09 23:11:03 +1000

    added title screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void LoadLevel (string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame() {
+        Application.Quit ();
+    }
+}
