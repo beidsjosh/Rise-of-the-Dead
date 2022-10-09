9b21789b919f587d86358a20b43d380ef2266535
Joshua <joshua.beidham@griffithuni.edu.au>
2022-10-10 00:41:58 +1000

    removed final score text

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,23 +6,6 @@ using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour {
 
-    public Text scoreText;
-    //private int score = 0;
-    //protected int scorePlayer = .score;
-    //public GameObject player = GameObject.GetComponent<playerScript>();
-    //public playerScript PlayerScript;
-    //public GameObject PlayerScript;
-
-    /*void Start(){
-        //playerScript = GameObject.Find("Player").GetComponent<playerScript>();
-        GameObject PlayerScript = gameObject;
-        //playerScript player = GameObject.FindObjectOfType(typeof(playerScript)) as Player;
-        playerScript player = (playerScript) PlayerScript.GetComponent(typeof(playerScript));
-        score = player.score;
-        UpdateScore(score);
-        //UpdateScore(playerScript.score);
-    }*/
-
     public void LoadLevel (string levelName) {
         SceneManager.LoadScene(levelName);
     }
@@ -30,10 +13,4 @@ public class MainMenu : MonoBehaviour {
     public void QuitGame() {
         Application.Quit ();
     }
-
-    /*public void UpdateScore(int addScore) {
-		// update the GUI score here
-		//scoreText.text = "Final Score!!: " + score.ToString();
-
-	}*/
 }
